e3fbadb4f703ecf6a61cba199c679c7c3879b833
h4zzkR <tramvenus@yandex.ru>
2020-05-31 14:19:04 +0300

    problem with single bert on two models: will be fixed soon

--- core/nlu/semantic_taggers/RateBook/trainer.py
+++ core/nlu/semantic_taggers/RateBook/trainer.py
@@ -9,14 +9,14 @@ from pathlib import Path
 import argparse
 import os
 import datetime
-from ..nlu_data.utils import DatasetLoader, encode_dataset, encode_token_labels
+from ...nlu_data.utils import DatasetLoader, encode_dataset, encode_token_labels
 from transformers import BertTokenizer
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.python.client import device_lib
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
-from .base import RequestIntentClassifier
+from .base import SemanticTagsExtractor
 
 
 parser = argparse.ArgumentParser(description='Trainer for bert-based intent classificator')
@@ -32,7 +32,7 @@ args = parser.parse_args()
 
 class ModelTrainer():
 
-    def __init__(self, max_length, curdir, name='intents_classifier', dset_name='merged',
+    def __init__(self, max_length, curdir, name='tag_extractor', dset_name='merged',
                  bert_model_name="bert-base-cased", model_save_dir='model'):
 
         self.training_model = name
@@ -44,54 +44,59 @@ class ModelTrainer():
 
         self.dataset_preload(dset_name, curdir, tokenizer, max_length)
 
-        self.model = RequestIntentClassifier(
-                    intent_num_labels=len(self.intent2id),
+        self.model = SemanticTagsExtractor(
+                    tag_num_labels=len(self.tag_map),
                     bert_model=bert_model_name,
                     )
-                    
 
         opt = Adam(learning_rate=3e-5, epsilon=1e-08)
-        losses = [SparseCategoricalCrossentropy(from_logits=True)]
+        loss = SparseCategoricalCrossentropy(from_logits=True)
+        metrics = SparseCategoricalAccuracy('accuracy')
 
-        metrics = [SparseCategoricalAccuracy('accuracy')]
-        self.model.compile(optimizer=opt, loss=losses, metrics=metrics)
+        self.model.compile(optimizer=opt, loss=loss, metrics=metrics)
 
 
     def dataset_preload(self, dset_name, curdir, tokenizer, max_length):
         d = DatasetLoader(dset_name)
         df_train, df_valid, df_test, \
-                self.intent2id, self.id2intent, tag2id, id2tag = d.load_prepare_dataset(curdir)
+                intent2id, id2intent, self.tag2id, self.id2tag = d.load_prepare_dataset(curdir)
 
-        self.intent_train = df_train["intent_label"].map(self.intent2id).values
-        self.intent_valid = df_valid["intent_label"].map(self.intent2id).values
-        self.intent_test = df_test["intent_label"].map(self.intent2id).values
-
+        print('Encoding data...')
         self.encoded_train = encode_dataset(tokenizer, df_train["words"], max_length)
         self.encoded_valid = encode_dataset(tokenizer, df_valid["words"], max_length)
         self.encoded_test = encode_dataset(tokenizer, df_test["words"], max_length)
+        
+        self.tag_train = encode_token_labels(
+            df_train["words"], df_train["word_labels"], tokenizer, self.tag2id, max_length)
+        self.tag_valid = encode_token_labels(
+            df_valid["words"], df_valid["word_labels"], tokenizer, self.tag2id, max_length)
+        self.tag_test = encode_token_labels(
+            df_test["words"], df_test["word_labels"], tokenizer, self.tag2id, max_length)
 
 
     def train(self, epochs, batch_size):
-
         time = datetime.datetime.now()
-        name = f"intents_cls_e{epochs}_bs{batch_size}"
+        name = f"tags_extractor_e{epochs}_bs{batch_size}"
 
         checkpoint_path = os.path.join(self.model_save_dir, name)
 
         # Create a callback that saves the model's weights
         cp_callback = ModelCheckpoint(filepath=checkpoint_path,
-                                        save_weights_only=True,
+                                        save_weights_onTaggery=True,
                                         verbose=1)
 
 
-        history = self.model.fit(self.encoded_train,
-                        self.intent_train, epochs=epochs, batch_size=batch_size,
-                        validation_data=(self.encoded_valid, self.intent_valid),
+        history = self.model.fit(self.encoded_train, self.tag_train,
+                        epochs=epochs, batch_size=batch_size,
+                        validation_data=(self.encoded_valid, self.tag_valid),
                         callbacks=cp_callback)
+        return self.model
+
 
 
 
 if __name__ == "__main__":
+    # python -m core.nlu.semantic_taggers.tagger.trainer
     curdir = Path(__file__).parent.absolute()
     trainer = ModelTrainer(args.max_length,\
          curdir, bert_model_name=args.model_name, dset_name=args.dataset)
