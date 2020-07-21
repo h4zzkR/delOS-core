066df0524f974bd19811b91cffe8c84db056d165
h4zzkR <tramvenus@yandex.ru>
2020-07-21 11:31:53 +0300

    delete folder

--- core/nlu/engine/default_engine.py
+++ core/nlu/engine/default_engine.py
@@ -14,6 +14,7 @@ class NLUEngine(NLUEngine):
         d = DatasetLoader(NLU_CONFIG['intents_dataset'])
         self.intent2id, self.id2intent = d.load_intents_map()
         self.tag2id, self.id2tag = d.load_tags_map()
+        del d
 
         self.featurizer = SenteceFeaturizer(NLU_CONFIG['featurizer_model'])
         self.classifier = IntentClassifier(self.id2intent, NLU_CONFIG['classifier_model'])
